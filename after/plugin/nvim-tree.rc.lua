3883bd5aa222294b03485f1535e3d0a55f48877e
Facu <facundo@kelsus.com>
2022-10-26 16:07:12 -0300

    Some modifications

--- after/plugin/nvim-tree.rc.lua
+++ after/plugin/nvim-tree.rc.lua
@@ -2,7 +2,7 @@ require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
     adaptive_size = true,
-    centralize_selectio = true,
+    centralize_selection = true,
     mappings = {
       list = {
         { key = "u", action = "dir_up" },
--- lua/base.lua
+++ lua/base.lua
@@ -27,7 +27,7 @@ vim.opt.si = true -- Smart indent
 vim.opt.wrap = false -- No wrap lines
 vim.opt.backspace = 'start,eol,indent'
 vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
-vim.opt.wildignore:append { '*/node_modules/*' } 
+-- vim.opt.wildignore:append { '*/node_modules/*' } 
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
